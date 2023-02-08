b91195112f6e26183f35f695877d2c79e7eadb9c
David Erickson <parker@Davids-MacBook-Pro.local>
2023-02-07 17:13:29 -0700

    Add guessing game exercise.

---
+++ guessing-game/target/debug/deps/guessing_game-79e68b6f2ab82b8c.d
@@ -0,0 +1,5 @@
+/Users/parker/dev/rust/rustbook/guessing-game/target/debug/deps/guessing_game-79e68b6f2ab82b8c.rmeta: src/main.rs
+
+/Users/parker/dev/rust/rustbook/guessing-game/target/debug/deps/guessing_game-79e68b6f2ab82b8c.d: src/main.rs
+
+src/main.rs:
---
+++ guessing-game/target/debug/deps/guessing_game-8c802c683bc0bed2.d
@@ -0,0 +1,5 @@
+/Users/parker/dev/rust/rustbook/guessing-game/target/debug/deps/guessing_game-8c802c683bc0bed2.rmeta: src/main.rs
+
+/Users/parker/dev/rust/rustbook/guessing-game/target/debug/deps/guessing_game-8c802c683bc0bed2.d: src/main.rs
+
+src/main.rs:
